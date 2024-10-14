18c7b1d3cd29f095a40aa275fcc845a68cebce79
Luan Martins <muanlartins@gmail.com>
2024-10-14 15:05:31 -0300

    feat: attempt to simplify path and fix .ts issues

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ The types to be shared between codepedia and codepedia-api.
 
 ## Deploy
 
-Make sure you are `npm login`
+Make sure you are `npm login` and have committed the changes
 Update version with `npm version major`, `npm version minor` or `npm version patch`
 Finally publish with `npm publish`
---
+++ enums/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ElementType = void 0;
+var ElementType;
+(function (ElementType) {
+    ElementType["snippet"] = "snippet";
+    ElementType["solution"] = "solution";
+})(ElementType || (exports.ElementType = ElementType = {}));
